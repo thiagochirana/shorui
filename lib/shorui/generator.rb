5083227df54b9c2f50ed7ebcd9ffe153fcf5705b
Chirana <engsw.thiago@gmail.com>
2025-01-20 08:07:24 -0300

    feat: add features to generate

--- lib/shorui/generator.rb
+++ lib/shorui/generator.rb
@@ -1,29 +1,59 @@
 module Shorui
   class Generator
-    def self.fetch_routes
+    def self.fetch_routes(ignore_rails_paths = false)
       Rails.application.routes.routes.map do |route|
-        {
-          path: route.path.spec.to_s,
-          verb: route.verb,
-          controller_action: route.defaults.slice(:controller, :action),
-        }
-      end.reject { |r| r[:path].include?('(.:format)') }
-    end
+        path = route.path.spec.to_s.gsub("(.:format)", "")
+        
+        if ignore_rails_paths && path.start_with?("/rails")
+          next 
+        end
+    
+        controller = route.defaults.dig(:controller)
+        action = route.defaults.dig(:action)
+    
+        if controller && action
+          {
+            path: path,
+            verb: route.verb,
+            controller_action: { controller: controller, action: action },
+            required_params: route.path.required_names,
+            optional_params: route.defaults.keys - [:controller, :action]
+          }
+        end
+      end.compact
+    end   
 
-    def self.generate_readme
-      routes = fetch_routes
-      content = "# API Documentation\n\n"
+    def self.generate_readme(ignore_rails_paths = false)
+      routes = fetch_routes(ignore_rails_paths)
+      dir_name = File.basename(Dir.pwd)
+      formatted_name = dir_name.gsub(/[-_\/|\\.]/, ' ').split.map(&:capitalize).join(' ')
+      content = "# #{formatted_name} API Documentation\n\n"
 
       routes.each do |route|
-        controller = route[:controller_action][:controller]
-        action = route[:controller_action][:action]
-        content << "## #{controller.camelize}##{action}\n"
-        content << "- **Path:** `#{route[:path]}`\n"
-        content << "- **HTTP Verb:** `#{route[:verb]}`\n\n"
-      end
+        if route.nil?
+          next
+        end
 
+        if ignore_rails_paths
+          next if route[:verb].start_with?("/rails")
+        end
+
+        controller = route.dig(:controller_action, :controller)
+        action = route.dig(:controller_action, :action)
+
+        if controller && action
+          required_params = route[:required_params].any? ? route[:required_params].join(', ') : nil
+          optional_params = route[:optional_params].any? ? route[:optional_params].join(', ') : nil
+    
+          content << "#### [#{route[:verb]}] #{route[:path]}\n"
+          content << "- **Controller#Action:** `#{controller}##{action}`\n"
+          content << "- **Required Params:** `#{required_params}`\n" if required_params
+          content << "- **Optional Params:** `#{optional_params}`\n\n" if optional_params
+        end
+      end
+    
       File.write('README.md', content)
       puts "README.md successfully generated in #{Dir.pwd}!"
-    end
+    end    
   end
 end
